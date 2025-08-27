96281cd9219da3435dd7b88dac37c60ef2a98c77
davidli9410 <david.li9410@gmail.com>
2025-08-26 21:58:38 -0400

    created basic REPL tester class, works with live user input. Fixed some display clarity issues with defining variables

---
+++ REPL_tester.py
@@ -0,0 +1,31 @@
+from Interpreter import Interpreter
+from Parser import Parser
+
+
+class REPL_tester :
+    def __init__(self):
+        self.interpreter = Interpreter()
+        self.parser = Parser()
+    def start_test(self):
+        while True:
+            
+            expression = input()
+            
+            if expression:
+                result = self.interpreter.run(expression)
+                if "define" in self.parser.tokenize(expression):
+                    print(f"{self.parser.tokenize(expression)[1]} defined as: {result}")
+                else:
+                    print(f"Result of expression: {result}")
+
+
+
+
+
+if __name__ == "__main__":
+    print("\033[1m" + "This is the interactive REPL command line tester for my interpreter. At any time, press ^C to exit the REPL" )
+    print("\033[1m")
+
+    repl = REPL_tester()
+    repl.start_test()
+
